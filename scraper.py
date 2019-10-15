8386df1f4ce2eb48095b63b7ed79a0d92fe3918f
Sam <samc@txstate.edu>
2019-10-14 20:23:09 -0500

    Working for 3 inputs

--- scraper.py
+++ scraper.py
@@ -1,19 +1,25 @@
-#This file reads an html file and parses out relevant information on Texas State university professors
-
+#This code reads an html file and parses out relevant information for txst faculty
 import sys
 import re
 
-#Open input/output files
-fr = open("input1.html", 'r')
-page = fr.read()
-fw = open("output1.txt", 'w')
-
-#Regular Expression to get information
+##############################################################################
+#				Regular Expressions			     #
+##############################################################################
 regexName = '(Mrs|Mr|Dr)\.\s[A-Z]\w*[\s-]+[A-Z]\w*'
 regexEducation = 'Education</h3>\s+.+\s+.+<p>((.+?))</p>'
 regexResearchInterests = 'Research Interests</h3>\s+.+\s+.+<p>((.+?))</p>'
 regexEmail = 'user=([a-zA-Z0-9_]*)'
 regexWebpage = 'https?:\/\/[www.]*cs.txstate.edu\/~[a-zA-Z0-9_]*'
+
+
+##############################################################################
+#				HTML File #1				     #
+##############################################################################
+
+#Open input/output files
+fr = open("input1.html", 'r')
+page = fr.read()
+fr.close()
 
 #Parse for relevant information
 name = re.search(regexName, page)
@@ -23,6 +29,8 @@ email = re.search(regexEmail, page)
 webpage = re.search(regexWebpage, page)
 
 #Print information to text file
+fw = open("output1.txt", 'w')
+
 if not name:
 	fw.write('Name: N/A\n')
 else:
@@ -34,7 +42,7 @@ else:
 	fw.write('Education: ' + education.group(1) + '\n')
 
 if not researchInterests:
-	fw.write('Research Interests: N/A\n')
+	fw.write('Research Interests: N\A\n')
 else:
 	fw.write('Research Interests: ' + researchInterests.group(1) + '\n')
 
@@ -44,10 +52,106 @@ else:
 	fw.write('Email: ' + email.group(1) + '@txstate.edu' + '\n')
 
 if not webpage:
-	fw.write('Webpage: N/A\n')
+	fw.write('Webpage: N/A\n\n')
 else:
-	fw.write('Webpage: ' + webpage.group(0) + '\n')
+	fw.write('Webpage: ' + webpage.group(0) + '\n\n')
 
 #Close files
+fw.close()
+
+
+
+##############################################################################
+#				HTML File #2				     #
+##############################################################################
+
+#Open input/output files
+fr = open("input2.html", 'r')
+page = fr.read()
 fr.close()
+
+#Parse for relevant information
+name = re.search(regexName, page)
+education = re.search(regexEducation, page)
+researchInterests = re.search(regexResearchInterests, page)
+email = re.search(regexEmail, page)
+webpage = re.search(regexWebpage, page)
+
+#Print information to text file
+fw = open("output2.txt", 'w')
+
+if not name:
+	fw.write('Name: N/A\n')
+else:
+	fw.write('Name: ' + name.group(0) + '\n')
+
+if not education:
+	fw.write('Education: N/A\n')
+else:
+	fw.write('Education: ' + education.group(1) + '\n')
+
+if not researchInterests:
+	fw.write('Research Interests: N\A\n')
+else:
+	fw.write('Research Interests: ' + researchInterests.group(1) + '\n')
+
+if not email:
+	fw.write('Email: N/A\n')
+else:
+	fw.write('Email: ' + email.group(1) + '@txstate.edu' + '\n')
+
+if not webpage:
+	fw.write('Webpage: N/A\n\n')
+else:
+	fw.write('Webpage: ' + webpage.group(0) + '\n\n')
+
+#Close files
+fw.close()
+
+
+##############################################################################
+#				HTML File #3				     #
+##############################################################################
+
+#Open input/output files
+fr = open("input3.html", 'r')
+page = fr.read()
+fr.close()
+
+#Parse for relevant information
+name = re.search(regexName, page)
+education = re.search(regexEducation, page)
+researchInterests = re.search(regexResearchInterests, page)
+email = re.search(regexEmail, page)
+webpage = re.search(regexWebpage, page)
+
+#Print information to text file
+fw = open("output3.txt", 'w')
+
+if not name:
+	fw.write('Name: N/A\n')
+else:
+	fw.write('Name: ' + name.group(0) + '\n')
+
+if not education:
+	fw.write('Education: N/A\n')
+else:
+	fw.write('Education: ' + education.group(1) + '\n')
+
+if not researchInterests:
+	fw.write('Research Interests: N\A\n')
+else:
+	fw.write('Research Interests: ' + researchInterests.group(1) + '\n')
+
+if not email:
+	fw.write('Email: N/A\n')
+else:
+	fw.write('Email: ' + email.group(1) + '@txstate.edu' + '\n')
+
+if not webpage:
+	fw.write('Webpage: N/A\n\n')
+else:
+	fw.write('Webpage: ' + webpage.group(0) + '\n\n')
+
+#Close files
 fw.close()
